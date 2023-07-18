c5c94d1b37cd86c85b3c15119cc7f766b96d3e15
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-07-18 14:27:53 -0700

    updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - it print output to terminal
+ * Return: it reurn 0
+ */
+int main(void) /*simple code */
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
