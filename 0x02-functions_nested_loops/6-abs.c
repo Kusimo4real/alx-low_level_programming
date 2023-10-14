13a9165bb5b2736511e391251b8dbee874863647
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-10-14 11:26:13 -0700

    a function that prints an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *_abs - This function computes the absolute value of an integer
+  *@c: this is the return value for the program
+  *Return: This function return an integer
+  */
+
+int _abs(int c)
+{
+	/*Declare the variable*/
+	/*check if value is less than or equal to -1*/
+	/*Multiply the value by -1*/
+	/* then return the integer*/
+
+
+	if (c <= -1)
+	{
+		c *= -1;
+	}
+	return (c);
+}
