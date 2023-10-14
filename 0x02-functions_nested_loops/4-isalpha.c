f8ad11aa59d57a21f9911e45d0c2969a75240473
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-10-14 06:12:44 -0700

    a function to check for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_isalpha - this check for alphabetical order
+ * @c: is the variable
+ *Return: return the integer c
+ */
+
+int _isalpha(int c)
+{
+	/*condition to print*/
+	
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
