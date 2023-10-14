7a5ffec335d363d9b021cc6607439e898e5f79e9
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-10-13 19:33:47 -0700

    alx coding

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,32 @@
 *
 *Return: Always 0.
 */
+/**
+*	_putchar('_');
+*	_putchar('p');
+*	_putchar('u');
+*	_putchar('t');
+*	_putchar('c');
+*	_putchar('h');
+*	_putchar('a');
+*	_putchar('r');
+*	_putchar('\n');
+*
+*	return (0);
+* }
+*/
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char name[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+{
+	_putchar(name[i]);
+
+}
 	_putchar('\n');
 	return (0);
 }
