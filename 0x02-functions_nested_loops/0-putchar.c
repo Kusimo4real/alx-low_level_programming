aed1d67a901b6612eae42432b97ca2d76ec2bc43
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-10-13 19:43:57 -0700

    alx function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,21 +4,20 @@
 *main - print _putchar to the standard output
 *
 *Return: Always 0.
-*/
-/**
-*	_putchar('_');
-*	_putchar('p');
-*	_putchar('u');
-*	_putchar('t');
-*	_putchar('c');
-*	_putchar('h');
-*	_putchar('a');
-*	_putchar('r');
-*	_putchar('\n');
-*
-*	return (0);
-* }
-*/
+***
+ *	_putchar('_');
+ *	_putchar('p');
+ *	_putchar('u');
+ *	_putchar('t');
+ *	_putchar('c');
+ *	_putchar('h');
+ *	_putchar('a');
+ *	_putchar('r');
+ *	_putchar('\n');
+ *
+ *	return (0);
+ * }
+ */
 
 int main(void)
 {
