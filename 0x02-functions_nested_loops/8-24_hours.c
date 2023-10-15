9a1ef6b65b46a56c6d7e6308695f36619957aed1
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-10-15 15:38:51 -0700

    a day in the life of jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * jack_bauer - The function print every minute of the day of jack bauer,
+  * starting from 00:00 to 23:59 
+  * Return : This return nothing
+  */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	/*let the hour represent the i */
+	/*let the minute represent the j*/
+	/*condition for the for-loop to print the hour*/
+	for (i = 0 ; i < 24 ; i++)
+	{
+		/*condition to print the minutes*/
+		for (j = 0 ; j < 60 ; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
