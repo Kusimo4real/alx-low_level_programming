2dc76dba84bfdeeb6bca68aa96623ebfe9763e11
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-10-29 06:42:23 -0700

    a program that add and prints the sum of the even valued terms of a fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  *main - a program that prints the sum of the even valued number
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	int first_number = 0;
+	int second_number = 1;
+	int next = 0; 
+	int sum = 0 ;
+	int limit = 4000000;
+
+	for (next; next <= limit ; next++)
+	{
+		next = first_number + second_number;
+		first_number = second_number;
+		second_number = next;
+		if (next % 2 == 0)
+		{
+			sum = sum + next;
+		}
+	}
+	printf("%d", sum);
+	printf("\n");
+	return (0);
+}
