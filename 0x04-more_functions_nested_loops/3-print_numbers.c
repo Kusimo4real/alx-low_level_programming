d1ea49d076a038f6440de0da73936a56331cb696
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-10-30 17:19:55 -0700

    this program print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *print_numbers - this function print numbers fro 0-9
+  *Return: nothing
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i < '10' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
