886931890110d3df2c26ba8b5990a27c3b787297
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-10-30 17:02:04 -0700

    This function multiply the parameter passed into it

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *mul - This function multiply the two variable passed into it
+  *@a: This is the first variable passed into the function
+  *@b: This is the second variable passed into the function
+  *Return: a * b
+  */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
