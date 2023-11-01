a7dddfcad88346e956cb91fca4014921681cdd59
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-11-01 13:40:46 -0700

    This function print the # in form of a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * print_square - this function print # in form of a square
+  *@size:This is the input parameter passed into the function
+  *Return:nothing
+  */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			for (j = 0 ; j < size ; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
