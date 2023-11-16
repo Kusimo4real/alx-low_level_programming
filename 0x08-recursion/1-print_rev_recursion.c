726cadbc5cebeac87a3c90f89e9633c5b5fc5aad
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-11-15 17:10:44 -0800

    this function prints a strings backward

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _print_rev_recursion - This function print a strings backward
+ * @s: this is the parameter passed into the function
+ * Return: 0
+ * */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
