4c15dbc40dcdf5999ee198c0dda32d66f1929445
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-11-19 08:39:32 -0800

    this functionchanges the value of n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function takes a pointer to an int as  parameter
+ * and updates the alue it points to to 98
+ * @n: The is the argument the function takes in
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
