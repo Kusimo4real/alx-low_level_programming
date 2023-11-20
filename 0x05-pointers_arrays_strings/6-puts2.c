e25764a6850a3185f6e822f58c49c91ba996a4b3
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-11-20 13:22:24 -0800

    this function print a function and jump the next

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - this function print the first character
+ * and jump the next one
+ * @str: argument
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+	/*
+	*int len;
+	*
+	*for (i = 0; str[i] != '\0' ; i++)
+	*	len++;
+	*/
+	for (i = 0 ; str[i] != '\0' ; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
