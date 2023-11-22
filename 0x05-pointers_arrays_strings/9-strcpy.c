fae870a787f730e13a1e2aa2800c91adedbe4dfb
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-11-21 21:58:07 -0800

    this function print the content of the src to the destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - this function copy the content of the src to dest
+ * @dest = the destination to copy to
+ * @src: the src file to copy from
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
