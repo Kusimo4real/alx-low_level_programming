cea2e7d4c6b22d139ae7011085c4a947888673e9
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-12-12 09:05:42 -0800

    a function that concatenates a character to a string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _strncat - This concatenate tw string
+ * @dest:this is the destination parameter
+ * @src:this is the src destination
+ * @n:this is the length that the string will receive the src
+ * Return: dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
@@ -9,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
-	for (j = 0; src[j] != src[n]; j++ )
+	for (j = 0; src[j] != src[n]; j++)
 	{
 		dest[i] = src[j];
 		i++;
