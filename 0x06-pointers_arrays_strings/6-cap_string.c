6adb7ab7d90302d5d523bd5256ccd076aaea8f35
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-12-13 13:20:48 -0800

    this function capitalize each string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,17 +3,30 @@ char *cap_string(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)/*checking for space and a tab*/
 	{
-		if (s[i] == 32 || s[i] == 9 || s[i] == 10 || s[i] == ',' || s[i] == ';' || s[i] == '!' || s[i] == '?' || s[i] == '(' || s[i] == ')' || s[i] == '{' ||  s[i] == '}')
+		if (i == 0)
+		{
+			if (s[i] >= 97 && s[i] <= 122)
+				s[i] = s[i] - 32;
+			continue;
+		}
+		if (s[i] == 32 || s[i] == 9 )
 		{
 			i++;
 			if (s[i] >= 65 && s[i] <= 90)
 				s[i] = s[i];
 			else
 				s[i] = s[i] - 32;
+			continue;
+		}
+		if (s[i] == 10)
+		{
+			i++;
+			s[i] = s[i] - 32;
 		}
 	}
+
 	return (s);
 }
 
