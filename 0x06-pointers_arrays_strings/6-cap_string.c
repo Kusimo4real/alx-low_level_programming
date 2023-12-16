ab0391c0af30834eccf6a0093ec7368411c7425e
Kusimo4real <abdulsemiukusimo@gmail.com>
2023-12-15 21:15:49 -0800

    a function that capitalizes all word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,32 @@
-#include"main.h"
+#include "main.h"
+/**
+ * cap_string - This function capitalizes all string
+ * @s: the parameter of the function
+ * Return: a character
+ */
+
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)/*checking for space and a tab*/
+	char SW[] = " \t \n , ;.!?"() { } "; /*string literals*/
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0)
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 		{
-			if (s[i] >= 97 && s[i] <= 122)
-				s[i] = s[i] - 32;
-			continue;
-		}
-		if (s[i] == 32 || s[i] == 9 )
-		{
-			i++;
-			if (s[i] >= 65 && s[i] <= 90)
-				s[i] = s[i];
-			else
-				s[i] = s[i] - 32;
-			continue;
-		}
-		if (s[i] == 10)
-		{
-			i++;
 			s[i] = s[i] - 32;
 		}
+		for (j = 0; j < 18; j++)
+		{
+			if (SW[j] == s[i])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
 	}
-
 	return (s);
 }
-
-
