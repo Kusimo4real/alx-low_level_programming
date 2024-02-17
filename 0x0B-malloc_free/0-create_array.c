4798874d43a99c9ed40c1b02ea777067c981cce3
Kusimo4real <abdulsemiukusimo@gmail.com>
2024-02-17 07:22:35 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,9 +16,10 @@ char *create_array(unsigned int size, char c)
 	ptr = malloc(sizeof(char) * size);
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
+
+	if (ptr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
