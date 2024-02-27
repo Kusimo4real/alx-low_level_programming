7f604db4860ebacbb1f510080f2e18247cfa8da9
Kusimo4real <abdulsemiukusimo@gmail.com>
2024-02-26 16:19:38 -0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
+	unsigned int i;
 
 	ptr = malloc(nmemb * size);
 
@@ -19,5 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 	return (ptr);
 }
